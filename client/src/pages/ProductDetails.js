28e0390b28a9f744db946f0215c19a068a9e9310
Marouanemarhrani <marhranim@gmail.com>
2024-08-25 14:34:10 +0200

    Product Details

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -1,9 +1,10 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout/Layout';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import toast from 'react-hot-toast';
 import { useCart } from '../context/cart';
+import "./ProductDetails.css";
 
 const ProductDetails = () => {
   const params = useParams();
@@ -11,14 +12,13 @@ const ProductDetails = () => {
   const [cart, setCart] = useCart();
   const [relatedProducts, setRelatedProducts] = useState([]);
 
-  //initalp details
-  useEffect(() =>{
-    if(params?.slug) getProduct()
+  useEffect(() => {
+    if (params?.slug) getProduct();
   }, [params?.slug]);
-  //get Product
+
   const getProduct = async () => {
     try {
-      const {data} = await axios.get(
+      const { data } = await axios.get(
         `${process.env.REACT_APP_API}/api/products/product/slug/${params.slug}`
       );
       setProduct(data?.product);
@@ -27,10 +27,10 @@ const ProductDetails = () => {
       console.log(error);
     }
   };
-  //get similar products
+
   const getSimilarProduct = async (pid, cid) => {
     try {
-      const {data} = await axios.get(
+      const { data } = await axios.get(
         `${process.env.REACT_APP_API}/api/products/product/related/${pid}/${cid}`
       );
       setRelatedProducts(data?.products);
@@ -38,68 +38,82 @@ const ProductDetails = () => {
       console.log(error);
     }
   };
+
   return (
     <Layout>
-      <div className='row container mt-2'>
-        <div className='col-md-6'>
-        <img 
-          src={`${process.env.REACT_APP_API}/api/products/photoURL/${product._id}`}
-          className="card-img-top" 
-          alt={product.name}
-          style={{ width: '18rem' }}
-         />
+      <div className="dtls16-product-container">
+        <div className="dtls5 text-center">
+          <h3 className="dtls6">
+            {product?.category?.name} - {product.name}
+          </h3>
         </div>
-        <div className='col-md-6'>
-          <h1 className='text-center'>Product Details</h1>
-          <h6>Name : {product.name}</h6>
-          <h6>Description : {product.description}</h6>
-          <h6>Price : {product.price}</h6>
-          <h6>Category : {product?.category?.name}</h6>
-          <button
-            className='home21 btn btn-secondary ms-1'
-            onClick={() => {
-              setCart([...cart, product]);
-            toast.success('Item added to cart');
-            }}
-          >
-            ADD TO CART
-          </button>
+        <div className="dtls17-product-details">
+          <div className="dtls2">
+            <img
+              src={`${process.env.REACT_APP_API}/api/products/photoURL/${product._id}`}
+              className="dtls3"
+              alt={product.name}
+            />
+          </div>
+          <div className="dtls4">
+            <div className="dtls7-btn-container">
+              <h4 className="dtls8 text-success">{product.price} €</h4>
+              <button
+                className="dtls9 btn btn-dark btn-lg"
+                onClick={() => {
+                  setCart([...cart, product]);
+                  toast.success('Item added to cart');
+                }}
+              >
+                Add to cart
+              </button>
+            </div>
+            <div className="dtls10">
+              <h6 className="dtls11">{product.description}</h6>
+            </div>
+            <div className="dtls12 mt-3">
+              <p>Free delivery between 27th Aug and 28th Aug</p>
+              <p>Free return within 30 days</p>
+              <p>12 months warranty</p>
+            </div>
+          </div>
         </div>
       </div>
-      <hr />
-      <div className='row container'>
+      <div className="dtls13 row container">
         <h6>Similar products</h6>
         {relatedProducts.length < 1 && (
-        <p className='text-center'>No similar products found</p>
+          <p className="dtls14 text-center">No similar products found</p>
         )}
-        <div className='d-flex flex-wrap'>
-        {relatedProducts?.map((p) => (
-              <div className='card m-2' style={{ width: '18rem' }} key={p._id}>
-                <img
-                  src={`${process.env.REACT_APP_API}/api/products/photoURL/${p._id}`}
-                  className='card-img-top'
-                  alt={p.name}
-                />
-                <div className='card-body'>
-                  <h5 className='card-title'>{p.name}</h5>
-                  <p className='card-text'>{p.description.substring(0, 30)}...</p>
-                  <p className='card-text'>€ {p.price}</p>
-                  <button
-                    className='home21 btn btn-secondary ms-1'
-                    onClick={() => {
-                      setCart([...cart, p]);
-                      toast.success('Item added to cart');
-                    }}
-                  >
-                    ADD TO CART
-                  </button>
-                </div>
+        <div className="dtls15 d-flex flex-wrap">
+          {relatedProducts?.map((p) => (
+            <div className="card m-2" style={{ width: '18rem' }} key={p._id}>
+              <img
+                src={`${process.env.REACT_APP_API}/api/products/photoURL/${p._id}`}
+                className="card-img-top"
+                alt={p.name}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{p.name}</h5>
+                <p className="card-text">
+                  {p.description.substring(0, 30)}...
+                </p>
+                <p className="card-text">€ {p.price}</p>
+                <button
+                  className="btn btn-secondary"
+                  onClick={() => {
+                    setCart([...cart, p]);
+                    toast.success('Item added to cart');
+                  }}
+                >
+                  Add to cart
+                </button>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
+        </div>
       </div>
     </Layout>
-  )
+  );
 };
 
 export default ProductDetails;
