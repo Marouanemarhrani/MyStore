0f05b4795c4f63c2062efb8f09227528d9ad9834
Marouanemarhrani <marhranim@gmail.com>
2024-09-03 01:50:51 +0200

    commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Import routes
 import userRoute from './routes/userRoute.js';
@@ -26,6 +28,13 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
+// Handle __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 // Route handling
 app.use('/api/users', userRoute);
 app.use('/api/categories', categoryRoute);
@@ -33,6 +42,11 @@ app.use('/api/products', productRoute);
 app.use('/api/appointments', appointmentRoute);
 app.use('/api/services', serviceRoute);
 app.use('/api/brands', brandRoute);
+
+// Handle all other routes and return the React app
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Define port
 const PORT = process.env.PORT || 8080;
