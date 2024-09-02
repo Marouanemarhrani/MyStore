d8a47be93352bb2661aee75e5fc1e8a709684867
Marouanemarhrani <marhranim@gmail.com>
2024-09-02 21:23:19 +0200

    commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import { fileURLToPath } from 'url';
 
 // Import routes
 import userRoute from './routes/userRoute.js';
@@ -37,7 +38,9 @@ app.use('/api/brands', brandRoute);
 // Define port
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static("./client/build")); 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Start server
 app.listen(PORT, () => {
